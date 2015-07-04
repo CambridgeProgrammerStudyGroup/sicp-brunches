5d536445256b8dee6f075bb319b037f7b50b6af4
Brice Fernandes <brice.fernandes@gmail.com>
2015-07-04 10:40:32 +0100

    Add previous work cleanup

--- Chapter1.rkt
+++ Chapter1.rkt
@@ -288,6 +288,13 @@ b
 (define (cube-root-improve y x)
   (/ (+ (/ x (* y y)) (* 2 y)) 3.0))
 
+; define generic function which takes an improver function 'improve'
+; and a test function 'test?' whcih checks if the iteration is sufficiently good.
+; the 'test?' function must be of the form:
+; 
+;     (test? <previous-guess> <current-guess> original value)
+; 
+; and return a boolean value.
 (define (iterator improve test? prev-guess curr-guess x)
   (if (test? prev-guess curr-guess x) curr-guess
           (iterator improve test? curr-guess (improve curr-guess x) x)))
