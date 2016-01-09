24b8b4634d71747691476da00866b511a2344b82
Brice Fernandes <brice@sigmaforge.com>
2016-01-09 11:21:16 +0000

    Add test for exception raising, add display utils

--- brice/utils.scm
+++ brice/utils.scm
@@ -6,6 +6,21 @@
 (define (dec x) (- x 1))
 (define (square x) (* x x))
 
+(define (str . parts)
+  (define strParts (map ~a parts))
+  (apply string-append  strParts ))
+
+(define (prn . lines)
+  (for-each
+   (lambda (line) (display (str line "\n")))
+   lines))
+
+(define (title ti)
+  (let ((long (make-string 60 #\-)))
+  	(prn "" long ti long "")))
+
+(define (show smth)
+	(display (format "~a\n" smth)))
 
 (define (reporterr msg)
 	(display "ERROR: ")
@@ -25,6 +40,14 @@
   		(assert msg pass)
   		(cond ((not pass)
   			(display (format "    Expected ~a got ~a\n" a b))))))
+
+(define-syntax assert-raises-error
+	(syntax-rules ()
+		[(assertraises msg body) 
+			(with-handlers 
+				([exn:fail? (lambda (ex) (reportok msg))]) 
+				(begin body (reporterr msg)))]))
+
 
 (define (average a b)
 	(/ (+ a b) 2))
