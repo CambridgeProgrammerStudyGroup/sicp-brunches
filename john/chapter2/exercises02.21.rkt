7778641ee0ce6c71c3cbc5984531a6f644d96921
John Donnellan <John@Casper.local>
2016-02-21 09:46:24 +0000

    add Exercises 2.26 to 2.28

--- john/chapter2/exercises02.21.rkt
+++ john/chapter2/exercises02.21.rkt
@@ -168,3 +168,259 @@
    (str "    cdr->car->cdr->car->cdr->car->cdr->car->cdr->car->cdr->car: "
         (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr c)))))))))))))
    ))
+
+;#########################################################################
+;#########################################################################
+
+(ti "Exercise 2.26")
+
+; Exercise 2.26.  Suppose we define x and y to be two lists:
+; 
+; (define x (list 1 2 3))
+; (define y (list 4 5 6))
+; 
+; What result is printed by the interpreter in response to evaluating each
+; of the following expressions:
+; 
+; (append x y)
+; 
+; (cons x y)
+; 
+; (list x y)
+
+(prn
+ (str "Guess...")
+ (str "  (append x y): (1 2 3 (4 5 6))")
+ (str "  (cons x y):   ((1 2 3) (4 5 6))")
+ (str "  (list x y):   ((1 2 3) (4 5 6))"))
+
+(let ((x (list 1 2 3))
+      (y (list 4 5 6)))    
+  (prn
+   (str)
+   (str "Actual...")
+   (str "  (append x y): " (append x y))
+   (str "  (cons x y)  : " (cons x y))
+   (str "  (list x y)  : " (list x y))
+   (str)
+   (str "Well that's embarrassing. One outa three IS bad.  I think I'll go")
+   (str "get another coffee :(")))
+
+
+;#########################################################################
+;#########################################################################
+
+(ti "Exercise 2.27")
+
+; Exercise 2.27.  Modify your reverse procedure of exercise 2.18 to
+; produce a deep-reverse procedure that takes a list as argument and
+; returns as its value the list with its elements reversed and with all
+; sublists deep-reversed as well. For example,
+; 
+; (define x (list (list 1 2) (list 3 4)))
+; 
+; x
+; ((1 2) (3 4))
+; 
+; (reverse x)
+; ((3 4) (1 2))
+; 
+; (deep-reverse x)
+; ((4 3) (2 1))
+
+(define (reverse lst)
+  (define (iter lst reversed)
+    (if (null? lst) reversed
+        (iter (cdr lst) (cons (car lst) reversed))))
+  (iter lst null))
+
+(define (deep-reverse lst)
+  (define (iter lst reversed)
+    (if (null? lst) reversed
+        (let* ((head (car lst))
+               (rev-head (if (pair? head)
+                             (iter head null)
+                             head)))
+          (iter (cdr lst) (cons rev-head reversed)))))
+  (iter lst null))
+
+(let ((x (list (list 1 2) (list 3 4))))
+  (prn
+   (str "x: " x)
+   (str "reverse x: " (reverse x))
+   (str "deep-reverse x: " (deep-reverse x))))
+    
+
+;#########################################################################
+;#########################################################################
+
+(ti "Exercise 2.28")
+
+; Exercise 2.28.  Write a procedure fringe that takes as argument a tree
+; (represented as a list) and returns a list whose elements are all the
+; leaves of the tree arranged in left-to-right order. For example,
+; 
+; (define x (list (list 1 2) (list 3 4)))
+; 
+; (fringe x)
+; (1 2 3 4)
+; 
+; (fringe (list x x))
+; (1 2 3 4 1 2 3 4)
+
+(define (fringe tree)
+  (if (pair? tree)
+      (append (fringe (car tree)) (fringe (cdr tree)))
+      (if (null? tree)
+           tree
+           (list tree))))
+
+(let ((x (list (list 1 2) (list 3 4)))) 
+  (prn
+   (str "x: " x)
+   (str "fringe x: " (fringe x))
+   (str "fringe list x x " (fringe (list x x)))))
+
+
+;#########################################################################
+;#########################################################################
+
+(ti "Exercise 2.29")
+
+; Exercise 2.29.  A binary mobile consists of two branches, a left branch
+; and a right branch. Each branch is a rod of a certain length, from which
+; hangs either a weight or another binary mobile. We can represent a
+; binary mobile using compound data by constructing it from two branches
+; (for example, using list):
+; 
+; (define (make-mobile left right)
+;   (list left right))
+; 
+; A branch is constructed from a length (which must be a number) together
+; with a structure, which may be either a number (representing a simple
+; weight) or another mobile:
+; 
+; (define (make-branch length structure)
+;   (list length structure))
+; 
+; a.  Write the corresponding selectors left-branch and right-branch,
+; which return the branches of a mobile, and branch-length and branch-
+; structure, which return the components of a branch.
+; 
+; b.  Using your selectors, define a procedure total-weight that returns
+; the total weight of a mobile.
+; 
+; c.  A mobile is said to be balanced if the torque applied by its top-
+; left branch is equal to that applied by its top-right branch (that is,
+; if the length of the left rod multiplied by the weight hanging from that
+; rod is equal to the corresponding product for the right side) and if
+; each of the submobiles hanging off its branches is balanced. Design a
+; predicate that tests whether a binary mobile is balanced.
+; 
+; d.  Suppose we change the representation of mobiles so that the
+; constructors are
+; 
+; (define (make-mobile left right)
+;   (cons left right))
+; (define (make-branch length structure)
+;   (cons length structure))
+; 
+; How much do you need to change your programs to convert to the new
+; representation?
+
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+(define one (make-mobile (make-branch 1 1) (make-branch 1 1)))
+(define two (make-mobile (make-branch 1 2) (make-branch 2 1)))
+(define three (make-mobile (make-branch 3 one) (make-branch 2 two)))
+(define oops (make-mobile (make-branch 3 one)
+                          (make-branch
+                           2
+                           (make-mobile (make-branch 2 2) (make-branch 2 1)))))
+
+(prn
+ "one:"
+ "    |"
+ "   -|- "
+ "  1   1"
+ ""
+ "two:"
+ "    |"
+ "   -|--"
+ "  2    1"
+ ""
+ "three:"
+ "          |"
+ "       ---|--"
+ "   -|-        -|--"
+ "  1   1      2    1"
+ ""
+ "oops:"
+ "          |"
+ "       ---|--"
+ "   -|-        --|--"
+ "  1   1      2     1"
+ )
+
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (car (cdr mobile)))
+
+(define (branch-length branch)
+  (car branch))
+
+(define (branch-structure branch)
+  (car (cdr branch)))
+
+(define (total-weight mobile)
+  (let* ((lb (left-branch mobile))
+         (rb (right-branch mobile))
+         (ll (branch-length lb))
+         (ls (branch-structure lb))
+         (rl (branch-length rb))
+         (rs (branch-structure rb)))
+    (+ (if (pair? ls) (total-weight ls) ls)
+       (if (pair? rs) (total-weight rs) rs))))
+
+(prn
+ ""
+ "Weights:"
+ (str "  one:   " (total-weight one))
+ (str "  two:   " (total-weight two))
+ (str "  three: " (total-weight three))
+ (str "  oops:  " (total-weight oops)))
+
+
+(define (branch-torque branch)
+  (* (branch-length branch)
+     (let ((structure (branch-structure branch)))
+       (if (pair? structure)
+           (total-weight structure)
+           structure))))
+
+(define (balanced? structure)
+  (if (not (pair? structure))
+      true
+      (and (equal? (branch-torque (left-branch structure))
+                  (branch-torque (right-branch structure)))
+           (and (balanced? (branch-structure (left-branch structure)))
+                (balanced? (branch-structure (right-branch structure)))))))
+
+(prn
+ (str "")
+ (str "Balanced: ")
+ (str "  one:   " (balanced? one))
+ (str "  two:   " (balanced? two))
+ (str "  three: " (balanced? three))
+ (str "  oops:  " (balanced? oops)))
+ 
+;#########################################################################
+;#########################################################################
+
+    
