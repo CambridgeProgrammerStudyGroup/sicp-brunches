f53d2c74f1901c420e66841ffde93bf0fa103958
John Donnellan <john@blueTribu.com>
2016-02-29 06:32:10 +0000

    add 2.38

--- john/chapter2/exercises2.33.rkt
+++ john/chapter2/exercises2.33.rkt
@@ -293,4 +293,63 @@
    (str "C: " C)
    (str "matrix-*-matrix B C:")
    (str "  expected: " (list '(22 28) '(49 64)))
-   (str "  actual:   " (matrix-*-matrix B C))))     
+   (str "  actual:   " (matrix-*-matrix B C))))
+
+;#########################################################################
+;#########################################################################
+
+(ti "Exercise 2.38")
+
+; Exercise 2.38.  The accumulate procedure is also known as fold-right,
+; because it combines the first element of the sequence with the result of
+; combining all the elements to the right. There is also a fold-left,
+; which is similar to fold-right, except that it combines elements working
+; in the opposite direction:
+; 
+; (define (fold-left op initial sequence)
+;   (define (iter result rest)
+;     (if (null? rest)
+;         result
+;         (iter (op result (car rest))
+;               (cdr rest))))
+;   (iter initial sequence))
+; 
+; What are the values of
+; 
+; (fold-right / 1 (list 1 2 3))
+; (fold-left / 1 (list 1 2 3))
+; (fold-right list nil (list 1 2 3))
+; (fold-left list nil (list 1 2 3))
+; 
+; Give a property that op should satisfy to guarantee that fold-right and
+; fold-left will produce the same values for any sequence.
+
+(prn
+ (str "Guess:")
+ (str "  (fold-right / 1 (list 1 2 3)):      1/6")
+ (str "  (fold-left / 1 (list 1 2 3)):       1 1/2")
+ (str "  (fold-right list nil (list 1 2 3)): (((1) 2) 3)")
+ (str "  (fold-left list nil (list 1 2 3)) : (1 (2 (3)))"))
+
+(define fold-right accumulate)
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(prn
+ (str)
+ (str "Actual")
+ (str "  (fold-right / 1 (list 1 2 3)):      " (fold-right / 1 (list 1 2 3)))
+ (str "  (fold-left / 1 (list 1 2 3)):       " (fold-left / 1 (list 1 2 3)))
+ (str "  (fold-right list nil (list 1 2 3)): " (fold-right list '() (list 1 2 3)))
+ (str "  (fold-left list nil (list 1 2 3)) : " (fold-left list '() (list 1 2 3))))
+
+(prn
+ (str)
+ (str "Yea!  I got 4 out of 0 right!  Maybe I should think instaead of guessing.")
+ (str "Or learn my left from right."))
