d2256830dcf4e7384071920f9d066cfc6ac9c540
John Donnellan <john@blueTribu.com>
2016-06-18 13:10:30 +0100

    add ex 2.56

--- john/chapter2/section2.3.2-ex56-58.rkt
+++ john/chapter2/section2.3.2-ex56-58.rkt
@@ -4,6 +4,12 @@
 
 (require "common.rkt")
 
+
+
+
+
+
+
 ;   Exercise 2.56
 ;   =============
 ;   
@@ -27,7 +33,122 @@
 
 (-start- "2.56")
 
+; Code from the text:
 
+(define (deriv-takes-makes exp var make-sum make-product)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv-takes-makes (addend exp) var make-sum make-product)
+                   (deriv-takes-makes (augend exp) var make-sum make-product)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv-takes-makes (multiplicand exp) var make-sum make-product))
+          (make-product (deriv-takes-makes (multiplier exp) var make-sum make-product)
+                        (multiplicand exp))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+  
+(define (variable? exp) (symbol? exp))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+(define (make-sum-simple a1 a2) (list '+ a1 a2))
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+(define (make-product-simple m1 m2) (list '* m1 m2))
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+
+(prn "Examples from text - simple makes...")
+(define (deriv-simple exp var) (deriv-takes-makes exp var make-sum-simple make-product-simple))
+(present-compare deriv-simple
+                 (list (list '(+ x 3) 'x) '(+ 1 0))
+                 (list (list '(* x y) 'x) '(+ (* x 0) (* 1 y)))
+                 (list (list '(* (* x y) (+ x 3)) 'x)
+                       '(+ (* (* x y) (+ 1 0)) (* (+ (* x 0) (* 1 y)) (+  x 3)))))
+
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (make-sum-simple a1 a2))))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (make-product-simple m1 m2))))
+
+(define (deriv exp var) (deriv-takes-makes exp var make-sum make-product))
+
+(prn "Examples from text - simplifying makes...")
+(present-compare deriv
+                 (list (list '(+ x 3) 'x) 1)
+                 (list (list '(* x y) 'x) 'y)
+                 (list (list '(* (* x y) (+ x 3)) 'x)
+                       '(+ (* x y) (* y (+ x 3)))))
+
+;; And the actual question...
+(define (exponent? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base exp)
+  (cadr exp))
+
+(define (exponent exp)
+  (caddr exp))
+
+(define (make-exponent base exponent)
+  (cond ((=number? exponent 0) 1)
+        ((=number? exponent 1) base)
+        (else (list '** base exponent))))
+
+
+
+(define (deriv-with-exp exp var) 
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv-with-exp (addend exp) var)
+                   (deriv-with-exp (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv-with-exp (multiplicand exp) var))
+          (make-product (deriv-with-exp (multiplier exp) var)
+                        (multiplicand exp))))
+        ((exponent? exp)
+         (make-product (exponent exp) (make-exponent (base exp) (make-sum (exponent exp) '-1))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+
+(present-compare deriv-with-exp
+                 (list (list '(** x 3) 'x)
+                       '(* 3 (** x 2)))
+                 (list (list '(** x 2) 'x)
+                       '(* 2 x))
+                 (list (list '(** x 1) 'x)
+                       '1)
+                 (list (list '(+ (* 7 (** x 4)) (* 5 (** x 2))) 'x)
+                       '(+ (* 7 (* 4 (** x 3))) (* 5 (* 2 x))) )
+                 )
 
 (--end-- "2.56")
 
