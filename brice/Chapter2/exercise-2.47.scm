2bb75d7217a513d3b1fa9a598d4c5871b5cb51de
Brice Fernandes <brice@sigmaforge.com>
2016-06-18 12:14:30 +0100

    Exercise 2.49

--- brice/Chapter2/exercise-2.47.scm
+++ brice/Chapter2/exercise-2.47.scm
@@ -2,6 +2,8 @@
 (require "../utils.scm")
 (require "./exercise-2.46.scm")
 
+(provide (all-defined-out))
+
 ; Exercise 2.47: 
 ; 
 ; Here are two possible constructors for frames:
@@ -15,16 +17,16 @@
 ; 
 ; origin-frame, edge1-frame, and edge2-frame
 
-(define (frameA origin edge1 edge2) 
+(define (frame origin edge1 edge2) 
 	(list origin edge1 edge2))
 
-(define (origin-frameA f)
+(define (origin-frame f)
 	(first f))
 
-(define (edge1-frameA f)
+(define (edge1-frame f)
 	(second f))
 
-(define (edge2-frameA f)
+(define (edge2-frame f)
 	(third f))
 
 (define (frameB origin edge1 edge2) 
@@ -42,14 +44,14 @@
 
 (let* 
 	(
-		(A (frameA (make-vect 1 2) (make-vect 3 4) (make-vect 5 6)))
+		(A (frame (make-vect 1 2) (make-vect 3 4) (make-vect 5 6)))
 		(B (frameB (make-vect 1 2) (make-vect 3 4) (make-vect 5 6)))
 	)
 	(assert "we can get the origin of frames type A & B" 
-		(all-equal? (origin-frameA A) (origin-frameB B) (make-vect 1 2)))
+		(all-equal? (origin-frame A) (origin-frameB B) (make-vect 1 2)))
 	(assert "we can get the first edge of frames type A & B" 
-		(all-equal? (edge1-frameA A) (edge1-frameB B) (make-vect 3 4)))
+		(all-equal? (edge1-frame A) (edge1-frameB B) (make-vect 3 4)))
 	(assert "we can get the second edge of frames type A & B" 
-		(all-equal? (edge2-frameA A) (edge2-frameB B) (make-vect 5 6)))
+		(all-equal? (edge2-frame A) (edge2-frameB B) (make-vect 5 6)))
 	
 )
