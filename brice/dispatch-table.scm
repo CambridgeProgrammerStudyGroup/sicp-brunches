412afc160621b98333a63ddb92dc2c5af1e3d569
Brice Fernandes <brice@sigmaforge.com>
2016-10-15 13:14:46 +0100

    Add generic type utilities to the dispatch table module

--- brice/dispatch-table.scm
+++ brice/dispatch-table.scm
@@ -40,6 +40,30 @@
 (define get (operation-table 'lookup-proc))
 (define put (operation-table 'insert-proc!))
 
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error
+            "No method for these types -- APPLY-GENERIC"
+            (list op type-tags))))))
+
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum -- TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum -- CONTENTS" datum)))
+
+
 (module* main #f
   (assert "Nothing in an empty table" (not (get 'a 'b)))
   (void (put 'a 'b 123))
