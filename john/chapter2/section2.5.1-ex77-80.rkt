087ba7cc54693697d048a74c826c14090a56bb46
John Donnellan <john@blueTribu.com>
2016-12-10 12:02:46 +0000

    Exercise 2.78

--- john/chapter2/section2.5.1-ex77-80.rkt
+++ john/chapter2/section2.5.1-ex77-80.rkt
@@ -79,7 +79,22 @@ abstraction.
 
 (-start- "2.78")
 
+(define (attach-tag type-tag contents)
+  (if (= 'scheme-number type-tag)
+      contents
+      (cons type-tag contents)))
 
+(define (type-tag datum)
+  (cond
+    ((number? datum) 'number)
+    ((pair? datum) (car datum))
+    (else error "Bad tagged datum -- TYPE-TAG" datum)))
+
+(define (contents datum)
+  (cond
+    ((number? datum) datum)
+    ((pair? datum) (cdr datum))
+    (error "Bad tagged datum -- CONTENTS" datum)))
 
 (--end-- "2.78")
 
