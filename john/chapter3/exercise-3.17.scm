8bace984caf8f00445c35933d3a6df9a4e68e3af
John Donnellan <john@blueTribu.com>
2017-08-12 13:23:19 +0100

    Exercise 3.17

--- john/chapter3/exercise-3.17.scm
+++ john/chapter3/exercise-3.17.scm
@@ -18,7 +18,77 @@
 
 (-start- "3.17")
 
+(define (original-count-pairs x)
+  (if (not (pair? x))
+      0
+      (+ (original-count-pairs (car x))
+         (original-count-pairs (cdr x))
+         1)))
 
+
+(define (contains? list item)
+  (if (null? list)
+      #f
+      (if (eq? item (car list))
+          #t
+          (contains? (cdr list) item))))
+
+(define (get-have-seen)
+  (define seen-items '())
+  (lambda (item)
+    (if (contains? seen-items item)
+        #t
+        (begin
+          (set! seen-items (cons item seen-items))
+          #f))))
+                       
+
+(define (count-pairs item)
+  (define have-seen? (get-have-seen))
+  (define (iter item)
+    (if (or (not (pair? item))
+            (have-seen? item))
+        0
+        (+ 1 (iter (car item)) (iter (cdr item)))))
+  (iter item))
+        
+
+(define simple
+  (cons 'a
+        (cons 'a
+              (cons 'a '()))))
+
+(define four
+  (let ((last (cons 'a '())))
+    (cons 'a
+          (cons last
+                last))))
+
+(define seven
+  (let ((last (cons 'a '())))
+    (let ((middle (cons last last)))
+      (cons middle middle))))
+
+(define infinite
+  (let ((last (cons 'a '())))
+    (let ((first (cons 'a
+                       (cons 'a
+                             last))))
+      (set-cdr! last first)
+      first)))
+      
+(prn "Using 'original' count-pairs from previous question:")
+(display (original-count-pairs simple)) (display "\n")
+(display (original-count-pairs four))   (display "\n")
+(display (original-count-pairs seven))  (display "\n")
+(prn "<does not finish>")
+
+(prn "
+Using new count-pairs:")
+(display (count-pairs simple)) (display "\n")
+(display (count-pairs four))   (display "\n")
+(display (count-pairs seven))  (display "\n")
+(display (count-pairs infinite))  (display "\n")
 
 (--end-- "3.17")
 
