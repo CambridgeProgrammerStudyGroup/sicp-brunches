46769baadc90ee8a8293a50299c6a90e1fb553b4
John Donnellan <john@blueTribu.com>
2017-09-02 11:08:29 +0100

    Exercise 3.19

--- john/chapter3/exercise-3.19.scm
+++ john/chapter3/exercise-3.19.scm
@@ -17,6 +17,63 @@
 (-start- "3.19")
 
 
+; helpers for constructing the subject list
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (make-list list length)
+  (if (= 0 length)
+      list
+      (make-list (cons 'content list) (- length 1))))
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+; Construct subject lists:
+(define straight (make-list '() 1000))
+
+(define looped (make-cycle (make-list '() 1000)))
+
+(define straight->looped (make-list '() 1000))
+(set-cdr! (last-pair straight->looped) looped)
+
+
+; Cycle check:
+(define step-count 0)
+
+(define (contains-cycle list)
+  (set! step-count 0)
+  (define (iter item remembered-item count initial-count)
+    (set! step-count (+ 1 step-count))
+    (if (not (pair? item))
+        #f
+        (or
+         (eq? remembered-item item)
+         (if (= count 0)
+             (iter (cdr item) item (* 2 initial-count) (* 2 initial-count))
+             (iter (cdr item) remembered-item (- count 1) initial-count)))))
+  (iter list '() 0 8))
+
+
+; Test against subjects:
+
+(prn
+ (str "Check we get the right result for the empty list (as we're using the empty
+list as the 'remembered item':
+    Empty list:" (contains-cycle '()) "  (" step-count " steps)")
+ ""
+ (str "    Straight: " (contains-cycle straight) "  (" step-count " steps)")
+ ""
+ (str "    Looped: " (contains-cycle looped) "  (" step-count " steps)")
+ ""
+ (str "    Straight then Looped:  " (contains-cycle straight->looped) "  (" step-count " steps)")
+ "
+This uses constant space but will usually require more steps.  E.g. the
+simple loop of 1,000 items took 2,015 steps to complete.
+")
 
 (--end-- "3.19")
 
