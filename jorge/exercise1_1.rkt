1b1ea8a70b55f82c0dd09607d09679383c1bb20a
jgart <jgart@dismail.de>
2019-01-26 12:26:17 +0000

    I completed the first exercise in Chapter One

---
+++ jorge/exercise1_1.rkt
@@ -0,0 +1,42 @@
+#lang racket
+(+ 5 3 4)
+
+(- 9 1)
+
+(/ 6 2)
+
+(- 4 6)
+
+(+ (* 2 4) (- 4 6))
+
+(define a 3)
+
+(define b (+ a 1))
+
+; b is equal to 4
+
+(+ a b (* a b))
+
+(= a b)
+
+(if (and (< b a) (< b (* a b)))
+    b
+    a)
+
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+(+ 2 (if (> b a) b a))
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))
+
+; b is equal to 4
+; a is equal to 3
+         
+
+
+
+
+
